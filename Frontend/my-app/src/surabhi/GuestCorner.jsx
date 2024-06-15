a25fc3782b06d1652c17a0ffe2a9c936ea33d2ba
SurabhiMoond <154314470+SurabhiMoond@users.noreply.github.com>
2024-06-15 22:55:03 +0530

    companyProfile_page_done

---
+++ Frontend/my-app/src/surabhi/GuestCorner.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const GuestCorner = () => {
+  return (
+    <div>
+      <img src="https://ibc-static.broad.msu.edu/sites/globaledge/blog/55748.jpg" alt="" />
+    </div>
+  )
+}
+
